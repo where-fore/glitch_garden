9eea4ab702bebf62706a98b843754c13fe343909
Dover <patrick_dover@yahoo.ca>
2019-05-23 09:13:17 -0400

    Added a (non-functional) main menu

---
+++ Glitch Garden/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    private string startMenuSceneString = "Start Menu";
+    private float timeBeforeLoadingStartMenu = 3f;
+
+    private int currentSceneIndex = 0;
+    
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        
+        if (currentSceneIndex == 0)
+        {
+            StartCoroutine(WaitThenLoadStartMenu());
+        }
+    }
+
+    private IEnumerator WaitThenLoadStartMenu()
+    {
+        yield return new WaitForSeconds(timeBeforeLoadingStartMenu);
+
+        LoadStartMenu();
+    }
+
+    public void LoadStartMenu()
+    {
+        SceneManager.LoadScene(startMenuSceneString);
+    }
+}
