b1601acb7170e5230bf83d4f9fe428ad86adf793
Dover <patrick_dover@yahoo.ca>
2019-06-02 18:08:49 -0400

    added damage functionality! things take damage and explode

---
+++ Glitch Garden/Assets/Scripts/DamageDealer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageDealer : MonoBehaviour
+{
+    [SerializeField] private int damage = 100;
+    public int GetDamageValue() {return damage;}
+
+    [SerializeField] private bool shouldDestroyPlayers = false;
+    public bool GetDestroyPlayerBool() {return shouldDestroyPlayers;}
+
+    [SerializeField] private bool shouldDestroyEnemies = false;
+    
+    public bool GetDestroyEnemiesBool() {return shouldDestroyEnemies;}
+
+    private string playerTag = "Player";
+    private string enemyTag = "Enemy";
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        DamageReceiver damageReceiver = other.gameObject.GetComponent<DamageReceiver>();
+        if (!damageReceiver) {return;}
+
+
+        if (other.gameObject.tag == playerTag)
+        {
+            if (shouldDestroyPlayers)
+            {
+                damageReceiver.TakeDamage(damage);
+                
+                Impact();
+            }
+        }
+
+        if (other.gameObject.tag == enemyTag)
+        {
+            if (shouldDestroyEnemies)
+            {
+                damageReceiver.TakeDamage(damage);
+
+                Impact();
+            }
+        }
+    }
+
+
+    public void Impact()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Glitch Garden/Assets/Scripts/DamageReceiver.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageReceiver : MonoBehaviour
+{
+    [SerializeField] private int health = 500;
+    public int GetHealth() {return health;}
+
+    [SerializeField] private GameObject deathVFX = null;
+    [SerializeField] float deathVFXDuration = 1f;
+    private SpriteRenderer mySpriteRenderer = null;
+    private float colourFlashLength = 0.25f;
+
+    private void Start()
+    {
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        StartCoroutine(FlashRed());
+
+        if (health <= 0)
+        {
+            Die();
+        }
+
+    }
+
+    private IEnumerator FlashRed()
+    {
+        mySpriteRenderer.color = Color.red;
+
+        yield return new WaitForSeconds(colourFlashLength);
+
+        mySpriteRenderer.color = Color.white;
+    }
+
+    private void Die()
+    {
+        GameObject deathVFXObject = Instantiate(deathVFX, transform.position, transform.rotation);
+        Destroy(deathVFXObject, deathVFXDuration);
+
+        Destroy(gameObject);
+    }
+
+}
