62bf0dba6c8d8346ba07c2a3e0b49253ae1b6d57
Dover <patrick_dover@yahoo.ca>
2019-06-03 12:38:53 -0400

    Added defender selector ui, cleaned up hierarchy

---
+++ Glitch Garden/Assets/Scripts/Defender.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Defender : MonoBehaviour
+{
+    [SerializeField] private int starCost = 100;
+    public int GetStarCost() {return starCost;}
+}
---
+++ Glitch Garden/Assets/Scripts/DefenderButton.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderButton : MonoBehaviour
+{
+    private Color notSelectedColor = new Color32(65, 65, 65, 255);
+    private Color selectedColor = new Color32(255, 255, 255, 255);
+
+    [SerializeField] GameObject defenderToSpawn = null;
+    public GameObject correspondingDefender() {return defenderToSpawn;}
+        
+    private bool selected = false;
+    private List<DefenderButton> allButtons = new List<DefenderButton>();
+    private SpriteRenderer mySpriteRenderer = null;
+
+    private void Start()
+    {
+        allButtons.AddRange(FindObjectsOfType<DefenderButton>());
+        mySpriteRenderer = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        if (selected)
+        {
+            mySpriteRenderer.color = selectedColor;
+        }
+        else
+        {
+            mySpriteRenderer.color = notSelectedColor;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        SelectButton();
+    }
+
+
+    private void SelectButton()
+    {
+        foreach (DefenderButton button in allButtons)
+        {
+            button.Deselect();
+        }
+        
+        this.Select();
+    }
+
+    public void Select()
+    {
+        selected = true;
+    }
+
+    public void Deselect()
+    {
+        selected = false;
+    }
+}
